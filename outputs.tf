cea56015d9169e2222d506f92d433b824bfb9c17
argp <syuo7@naver.com>
2020-04-16 13:28:33 +0900

    add modify outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "aws_elb_address" {
-  description = "This address is dns name of the ELB"
-  value = "${aws_elb.elb.dns_name}"
+output "aws_alb_address" {
+  description = "This address is dns name of the ALB"
+  value = "${aws_alb.alb.dns_name}"
 }
